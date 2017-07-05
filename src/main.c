1d6e29af4fe23027ddf16d92f07d08f270ae7fe6
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2017-07-06 00:10:51 +0500

    styleguide configs for c-files

--- src/main.c
+++ src/main.c
@@ -11,18 +11,18 @@ extern void __stginit_Bar(void);
 
 int main(int argc, char *argv[])
 {
-	int i;
-	hs_init(0, NULL);
+  int i;
+  hs_init(0, NULL);
 #ifdef __GLASGOW_HASKELL__
-	hs_add_root(__stginit_Foo);
-	hs_add_root(__stginit_Bar);
+  hs_add_root(__stginit_Foo);
+  hs_add_root(__stginit_Bar);
 #endif
 
-	i = fooForeign(25);
-	printf("fooForeign of 25: %d\n", i);
-	i = barForeign(25);
-	printf("barForeign of 25: %d\n", i);
+  i = fooForeign(25);
+  printf("fooForeign of 25: %d\n", i);
+  i = barForeign(25);
+  printf("barForeign of 25: %d\n", i);
 
-	hs_exit();
-	return 0;
+  hs_exit();
+  return 0;
 }
