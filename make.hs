39c872e2cfe2140c7d82d4750a244b9c6bfb8f75
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2017-07-06 02:59:03 +0500

    building shared libraries too

--- make.hs
+++ make.hs
@@ -3,6 +3,7 @@
  --resolver=lts-8.21
  --package base-unicode-symbols
  --package directory
+ --package filepath
  --package process
  --package lens
  -}
@@ -16,14 +17,24 @@ import "base-unicode-symbols" Prelude.Unicode
 import "base" System.Exit (die, exitWith, ExitCode (ExitSuccess))
 import "base" System.Environment (getArgs)
 import "base" System.IO.Error (isDoesNotExistError, catchIOError, ioError)
-import "directory" System.Directory (createDirectory, removeDirectoryRecursive)
+import "directory" System.Directory (createDirectoryIfMissing, removeDirectoryRecursive)
 import "process" System.Process (createProcess, proc, waitForProcess, ProcessHandle)
+import "filepath" System.FilePath ((</>), (<.>))
 
 import "base" Control.Monad (forM_)
 import "lens" Control.Lens (_4, (^.))
 
 import "base" Data.List (find)
 import "base" Data.Maybe (fromMaybe)
+import "base" Data.Char (toLower)
+
+
+srcDir, buildDir, distDir, buildAppDir, buildLibDir ∷ String
+srcDir      = "src"
+buildDir    = "build"
+distDir     = "dist"
+buildAppDir = buildDir </> "app"
+buildLibDir = buildDir </> "shared-library"
 
 
 main ∷ IO ()
@@ -41,31 +52,47 @@ main = fmap (\x → if length x ≡ 0 then ["build"] else x) getArgs
 
 
 cleanTask ∷ IO ()
-cleanTask = removeDirectoryRecursive "build" `catchIOError`
-              \e → if isDoesNotExistError e then pure () else ioError e
+cleanTask = forM_ [buildDir, distDir] $ \dir →
+  removeDirectoryRecursive dir `catchIOError`
+    \e → if isDoesNotExistError e then pure () else ioError e
 
 
 buildTask ∷ IO ()
 buildTask = do
 
   exec "stack" ["build", "--only-dependencies"]
-  createDirectory "build"
+  createDirectoryIfMissing True buildAppDir
+  createDirectoryIfMissing True distDir
 
   forM_ ["Baz/Qux", "Foo", "Bar"] $ \x →
-    runGhc ["-c", "-O", "src/" ⧺ x ⧺ ".hs", "-ibuild", "-outputdir", "build", "-Wall", "-O2"]
+    runGhc [ "-c", "-O", srcDir </> x <.> "hs"
+           , "-i" ⧺ buildAppDir, "-outputdir", buildAppDir
+           , "-Wall", "-O2", "-optc-O2"
+           ]
 
-  runGhc [ "--make", "-no-hs-main", "-optc-O2", "-optc-O", "src/main.c"
-         , "build/Foo.o", "build/Bar.o"
-         , "-outputdir", "build", "-o", "build/main"
-         ]
+  runGhc $ let ob x = buildAppDir </> x <.> "o"
+            in [ "--make", "-no-hs-main", "-optc-O2"
+               , "-optc-O", srcDir </> "main" <.> "c", ob "Foo", ob "Bar"
+               , "-outputdir", buildAppDir, "-o", distDir </> "app"
+               ]
 
+  createDirectoryIfMissing True buildLibDir
 
--- TODO shared library
--- ghc --make -dynamic -shared -fPIC Foo.hs -o libfoo.so
+  forM_ ["Baz/Qux", "Foo", "Bar"] $ \x →
+    runGhc [ "-c", "-O", srcDir </> x <.> "hs"
+           , "-i" ⧺ buildLibDir, "-outputdir", buildLibDir
+           , "-Wall", "-O2", "-dynamic", "-fPIC", "-optc-O2"
+           ]
+
+  forM_ ["Foo", "Bar"] $ \x →
+    runGhc [ "--make", "-dynamic", "-shared", "-fPIC", "-optc-O2"
+           , buildLibDir </> x <.> "o", "-outputdir", buildLibDir
+           , "-o", distDir </> "lib" ⧺ map toLower x <.> "so"
+           ]
 
 
 runTask ∷ IO ()
-runTask = exec "build/main" []
+runTask = exec (distDir </> "app") []
 
 exec ∷ FilePath → [String] → IO ()
 exec bin args = failCheck $ fmap (^. _4) $ createProcess $ proc bin args
---
+++ src/lib.c
@@ -0,0 +1,10 @@
+#include <HsFFI.h>
+
+#ifdef __GLASGOW_HASKELL__
+#include "Foo_stub.h"
+#include "Bar_stub.h"
+extern void __stginit_Foo(void);
+extern void __stginit_Bar(void);
+#endif
+
+
